75849cb7a81727f4b8a93db741917125527c32c3
ApremierA <anisimov.aleksey@itmh.ru>
2019-01-28 16:56:07 +0500

    Added PHP_SQL_Parser

---
+++ public/upload/InsertSQL.sql
@@ -0,0 +1,3 @@
+INSERT INTO `1parse_table` (`id`,`name`,`title`,`description`) VALUES (1,'a','a1','a2');
+INSERT INTO `2parse_table` (`id`,`name`,`title`,`description`) VALUES (2,'b','b1','b2');
+INSERT INTO `3parse_table` (`id`,`name`,`title`,`description`) VALUES (3,'c','c1','c2');
--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -10,6 +10,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use PHPSQLParser\PHPSQLParser;
 
 class IndexController extends AbstractController
 {
@@ -20,6 +21,30 @@ class IndexController extends AbstractController
     public function Index() : Response
     {
         return new Response('Index');
+    }
+
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function Parser() : Response
+    {
+        $parser = new PHPSQLParser();
+
+        $file_path = dirname(__DIR__) . '/../public/upload/InsertSQL.sql';
+        $file_handle = fopen($file_path, "r");
+        while (!feof($file_handle)) {
+            $line = fgets($file_handle);
+
+            $parsed = $parser->parse($line);
+
+            echo '<pre>';
+            print_r($parsed);
+            echo '</pre>';
+
+        }
+        fclose($file_handle);
+
+        return new Response('Parser');
     }
 
 }
